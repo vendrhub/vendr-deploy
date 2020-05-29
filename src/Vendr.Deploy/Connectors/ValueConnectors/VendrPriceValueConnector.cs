2bec473ebaeea5645abffc24ae7ac5764205ffcf
mattbrailsford <me@mattbrailsford.com>
2020-05-29 16:29:55 +0100

    Stubbed initial value connectors

---
+++ src/Vendr.Deploy/Connectors/ValueConnectors/VendrPriceValueConnector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Umbraco.Core.Deploy;
+using Umbraco.Core.Models;
+
+namespace Vendr.Deploy.Connectors.ValueConnectors
+{
+    public class VendrPriceValueConnector : IValueConnector
+    {
+        public IEnumerable<string> PropertyEditorAliases => new[] { "Vendr.Price" };
+
+        public object FromArtifact(string value, PropertyType propertyType, object currentValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToArtifact(object value, PropertyType propertyType, ICollection<ArtifactDependency> dependencies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Vendr.Deploy/Connectors/ValueConnectors/VendrStoreEntityPickerValueConnector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Umbraco.Core.Deploy;
+using Umbraco.Core.Models;
+
+namespace Vendr.Deploy.Connectors.ValueConnectors
+{
+    public class VendrStoreEntityPickerValueConnector : IValueConnector
+    {
+        public IEnumerable<string> PropertyEditorAliases => new[] { "Vendr.StoreEntityPicker" };
+
+        public object FromArtifact(string value, PropertyType propertyType, object currentValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToArtifact(object value, PropertyType propertyType, ICollection<ArtifactDependency> dependencies)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
