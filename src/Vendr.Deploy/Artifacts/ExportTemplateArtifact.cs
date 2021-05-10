f01ebd34cd790ea95041d40c01a862bbcea7422e
mattbrailsford <me@mattbrailsford.com>
2021-05-10 10:27:03 +0100

    Added export template support

---
+++ src/Vendr.Deploy/Artifacts/ExportTemplateArtifact.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core;
+using Umbraco.Core.Deploy;
+
+namespace Vendr.Deploy.Artifacts
+{
+    public class ExportTemplateArtifact : StoreEntityArtifactBase
+    {
+        public ExportTemplateArtifact(GuidUdi udi, GuidUdi storeUdi, IEnumerable<ArtifactDependency> dependencies = null)
+            : base(udi, storeUdi, dependencies)
+        { }
+
+        public int Category { get; set; }
+
+        public string FileMimeType { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public int ExportStrategy { get; set; }
+
+        public string TemplateView { get; set; }
+
+        public int SortOrder { get; set; }
+    }
+}
