c5790e7813a1768e68816ca1bf2ec084f3a6c523
mattbrailsford <me@mattbrailsford.com>
2023-04-27 11:03:21 +0100

    Update rounding decimal converter to check for decimal not double

--- src/Vendr.Deploy/Converters/RoundingDecimalJsonConverter.cs
+++ src/Vendr.Deploy/Converters/RoundingDecimalJsonConverter.cs
@@ -26,7 +26,7 @@ namespace Vendr.Deploy.Converters
             => false;
 
         public override bool CanConvert(Type objectType)
-            => objectType == typeof(double);
+            => objectType == typeof(decimal);
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
             => throw new NotImplementedException();
